dd0bddb65eef6e63d96d25ffc8da40abc719f4a3
Julie Morrison <jmor485@gmail.com>
2019-05-09 14:10:09 -0400

    resolving untracked content in submodules

---
+++ burger/db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('The All American');
+INSERT INTO burgers (burger_name) VALUES ('The Whopper');
+INSERT INTO burgers (burger_name) VALUES ('The Big Mac');
+INSERT INTO burgers (burger_name) VALUES ('The Baconator');
