f474ebc5654746bcf6fc8e300da052f48b125e53
Julie Morrison <jmor485@gmail.com>
2019-06-07 10:07:45 -0400

    app.js - doesn't work yet

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,27 +1,42 @@
 const app = {
-
     devourBurger: function (id) {
-        $.ajax("/api/burger/" + id, {
+        $.ajax({
+            url: `/api/burger/${id}`,
             type: "PUT",
+            success: function () {
+                window.location.reload();
+            }
         })
     },
 
-    burgerClicks: function () {
-        $(".devour-btn").on("click", function (event) {
-            var id = $(this).data("data-burger");
+    burgerActions: function () {
+        $(".devour-btn").on("click", function () {
+            let id = $(this).attr("data-burger");
+            app.devourBurger(id);
+        });
 
-            $.ajax("/api/burger" + id, {
-                type: "ADD",
-            }).then(
-                function () {
-                    location.reload();
+        $("#addBurger").on("click", function (e) {
+            e.preventDefault();
+            const data = {
+                name: $("#burgerName").val()
+            };
+            $.ajax({
+                type: "POST",
+                url: "api/burger",
+                data: data,
+                success: function (result) {
+                    console.log(result);
+                    window.location.reload();
                 }
-            )
-        })
-        app.devourBurger(id);
+            });
+        });
+    },
 
-        //ADD BURGER TO DEVOURED
+    init: function () {
+        this.burgerActions();
     }
-}
+};
 
-
+$(document).ready(function() {
+   app.init();
+});
